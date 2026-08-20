f142ab4db7e96fd3696358f2c36ded0ba84df078
Phoebe Wang <phoebe_wang@berkeley.edu>
2026-08-20 13:55:13 -0700

    Created a package with the newton.py file and the tests.

---
+++ mypkg/__init__.py
@@ -0,0 +1,4 @@
+## Make objects from mymod.py available as `mypkg.object_name`.
+from mypkg.mymod import *
+
+print("Welcome to my package.")
---
+++ mypkg/newton.py
@@ -0,0 +1,23 @@
+def derivative(f, x, h=1e-6):
+    """A function that returns the first derivative of a point x."""
+    return (f(x + h) - f(x)) / h
+
+
+def second_derivative(f, x, h=1e-6):
+    """A function that returns the second derivative of a point x."""
+    return (derivative(f, x + h) - derivative(f, x)) / h
+
+
+def optimize(f, x0, h=1e-6):
+    """A function that implements Newton's Method for optimization."""
+    x = x0
+
+    while True:
+        x_new = x - derivative(f, x, h) / second_derivative(f, x, h)
+
+        # Stopping criterion
+        if abs(x_new - x) < 1e-6:
+            break
+
+        x = x_new
+    return x_new
