a82ee80ae9e65840bd6eb0db8d74ac07818458f3
Phoebe Wang <phoebe_wang@berkeley.edu>
2026-08-20 10:02:57 -0700

    Implemented multivariate Newton's method.

---
+++ multivariate_newton.py
@@ -0,0 +1,30 @@
+import sympy as sp
+import numpy as np
+
+def gradient(f, x, y):
+    return sp.derive_by_array(f, (x, y))
+
+
+def hessian(f, x, y):
+    return sp.hessian(f, (x, y))
+
+
+def multivariate_method(f, x0, y0):
+    x, y = sp.symbols('x y')                # create symbolic variables
+    point = np.array([x0, y0], dtype=float) # starting point
+
+    while True:
+        g = gradient(f, x, y)
+        h = hessian(f, x, y)
+
+        g = np.array(g.subs({x: point[0], y: point[1]}), dtype=float)
+        h = np.array(h.subs({x: point[0], y: point[1]}), dtype=float)
+
+        point_new = point - np.dot(np.linalg.inv(h), g)
+
+        if np.linalg.norm(point_new - point) < 1e-6:
+            break
+
+        point = point_new
+    return point_new
+    
